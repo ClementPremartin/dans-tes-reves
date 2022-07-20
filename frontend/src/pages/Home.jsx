5fb406cb96c16ae66cb5405984e1f18f8a23c7a8
ClementPremartin <clementpremartin@gmail.com>
2022-07-20 17:08:07 +0200

    adding clouds in home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,20 @@
 import Presentation from "@components/Presentation";
 import Sections from "@components/Sections";
 
+import CloudRight from "@assets/cloud-right.png";
+import CloudMid from "@assets/cloud-mid.png";
+
 function Home() {
   return (
     <div className="m-8">
       <Presentation />
+      <div className="w-40 absolute right-0 pt-10 opacity-80">
+        <img src={CloudRight} alt="tiny cloud" />
+      </div>
       <Sections />
+      <div className="w-40 relative opacity-80">
+        <img src={CloudMid} alt="tiny cloud" />
+      </div>
     </div>
   );
 }
