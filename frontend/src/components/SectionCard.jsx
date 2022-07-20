e06ee3cacf1593ca3df11d4349c5070f9c62347b
ClementPremartin <clementpremartin@gmail.com>
2022-07-20 17:54:35 +0200

    setup containers for sections

--- frontend/src/components/SectionCard.jsx
+++ frontend/src/components/SectionCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function SectionCard({ card }) {
   return (
-    <div className="bg-darkBlue opacity-75 rounded-lg mb-10 shadow-2xl max-w-sm">
+    <div className="bg-darkBlue opacity-75 rounded-lg mb-20 shadow-2xl w-72">
       <Link to={`/home/${card.id}`}>
         <button type="button">
           <img
@@ -11,11 +11,11 @@ function SectionCard({ card }) {
               card.pictures[0].image_url
             }`}
             alt={`${card.pictures[0].image_alt}`}
-            className="opacity-100 rounded-t-lg w-full object-cover"
+            className="opacity-100 rounded-t-lg w-full object-fill"
           />
-          <h1 className="text-white font-sansita flex justify-center text-center p-5">
+          <p className="text-white font-sansita flex justify-center text-center p-5">
             {card.artTitle}
-          </h1>
+          </p>
         </button>
       </Link>
     </div>
