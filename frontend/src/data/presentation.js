e2efa7b143aec0425ba2aa0225fb69a26b1e573a
ClementPremartin <clementpremartin@gmail.com>
2022-07-20 13:22:53 +0200

    adding presentation data file

---
+++ frontend/src/data/presentation.js
@@ -0,0 +1,23 @@
+const presentation = [
+  {
+    id: 1,
+    description:
+      "Je raconte des histoires tissées par les songes et l’imaginaire. À travers mes créations, je me plais à révéler mon univers proche de la nature et des créatures hybrides.",
+  },
+  {
+    id: 2,
+    description:
+      "Avec des tendances oniriques, j’aime apporter du positif et de la folie aux spectateurs, faire vibrer leur âme de grand enfant, à la manière du Pays des Merveilles de L.Caroll.",
+  },
+  {
+    id: 3,
+    description:
+      "Pour cela, j’aime jouer avec les matières, questionner les relations qu’elles peuvent entretenir entre elles. Je tente de valoriser les matériaux qui m’entourent, ceux du coin de la table, intégrant notre quotidien.",
+  },
+  {
+    id: 4,
+    description: "Voici mon projet **Dans tes rêves**",
+  },
+];
+
+export default presentation;
