2a789733edc41a67456f5ccd3fc9333aa72ff62f
ClementPremartin <clementpremartin@gmail.com>
2022-07-20 09:54:10 +0200

    create routes and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable import/no-unresolved */
+import { Routes, Route } from "react-router-dom";
+
+import Admin from "@pages/Admin";
+import Waiting from "@pages/Waiting";
+import Home from "@pages/Home";
+import Serie from "@pages/Serie";
+import Contact from "@pages/Contact";
+
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <p>coucou</p>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/" element={<Waiting />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/:id" element={<Serie />} />
+        <Route path="/contact/:id" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
