5d471861702e27fb07a39ab65f9e883387343be2
ClementPremartin <clementpremartin@gmail.com>
2022-07-20 12:26:02 +0200

    adding a few colors in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: "#80A4BB",
+        yellow: "#E4DA64",
+        darkBlue: "#475C6A",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
