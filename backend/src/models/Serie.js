47fd05db2cfeb75dbb470969cd1f43153a8c4794
ClementPremartin <clementpremartin@gmail.com>
2022-07-21 12:32:23 +0200

    setup get serie name

---
+++ backend/src/models/Serie.js
@@ -0,0 +1,16 @@
+const AbstractManager = require("./AbstractManager");
+
+class SerieManager extends AbstractManager {
+  static table = "series";
+
+  findSection(id) {
+    return this.connection.query(
+      `SELECT s.id AS serie_id, s.name AS serie_name FROM ${SerieManager.table} AS s 
+      INNER JOIN creation ON c.series_id=s.id 
+      WHERE s.id=? LIMIT 1`,
+      [id]
+    );
+  }
+}
+
+module.exports = SerieManager;
