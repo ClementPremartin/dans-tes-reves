9d55d88dce77e085e080b063bab2feef70c4088a
ClementPremartin <clementpremartin@gmail.com>
2022-07-21 12:49:29 +0200

    setup creation manager to get creation by serie id

--- backend/src/models/CreationManager.js
+++ backend/src/models/CreationManager.js
@@ -5,9 +5,18 @@ class CreationManager extends AbstractManager {
 
   findSection() {
     return this.connection.query(
-      `SELECT c.id, c.art_title, s.id AS serie_id, s.name AS serie_name, f.image_url, f.image_alt from ${CreationManager.table} AS c
+      `SELECT c.id, c.art_title, s.id AS serie_id, s.name AS serie_name, f.image_url, f.image_alt FROM ${CreationManager.table} AS c
       LEFT JOIN files AS f ON f.id=c.files_id
       INNER JOIN series AS s ON s.id = c.series_id`
+    );
+  }
+
+  findCreation(id) {
+    return this.connection.query(
+      `SELECT c.id, c.art_title, c.date, c.size, c.technical, c.story, f.image_url, f.image_alt FROM ${CreationManager.table} AS c
+      LEFT JOIN files AS f ON f.id=c.files_id
+      WHERE c.series_id = ?`,
+      [id]
     );
   }
 }
