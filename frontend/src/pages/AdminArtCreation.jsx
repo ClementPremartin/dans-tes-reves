0d016fc7adbf1015ad2faa67c3de6e8657fc1699
ClementPremartin <clementpremartin@gmail.com>
2022-07-21 18:04:29 +0200

    adding tiny cloud on creation page

--- frontend/src/pages/AdminArtCreation.jsx
+++ frontend/src/pages/AdminArtCreation.jsx
@@ -3,11 +3,16 @@ import AddCreation from "@components/admin/AddCreation";
 import Footer from "@components/Footer";
 import Header from "@components/Header";
 
+import CloudMid from "@assets/cloud-mid.png";
+
 function AdminArtCreation() {
   return (
     <div>
       <Header />
       <div className="m-8">
+        <div className="w-48 relative opacity-80 cloud-mid">
+          <img src={CloudMid} alt="tiny cloud" />
+        </div>
         <h1 className="text-white font-sansita text-3xl pt-12 pb-12">
           Gestion des Créations
         </h1>
